d5370e1093c58e63a6d57a5f620cb2446fcf7fe8
zokistojcev <zorancho83>
2017-02-07 11:47:00 +0100

    zoki2

--- 2-6/script.ts
+++ 2-6/script.ts
@@ -1,4 +1,42 @@
-let dfggdfgtsc = () => {console.log('hi')}
-let dfg = () => {console.log('hi')}
-let dfgdfgd = () => {console.log('hi')}
-let dfgdfgdfg = () => {console.log('hi')}
+
+interface iListOfIIngredients{
+    flour: string;
+    milk: string;
+    oil: string;
+    salt: string;
+    sugar: string;
+    egg: string;
+    tomatoes: string;
+    pepper: string;
+    yellowCheese: string;
+    whiteCheese: string;
+}
+
+class ListOfIIngredients {
+    public flour: string;
+    public milk: string;
+    public oil: string;
+    public salt: string;
+    public sugar: string;
+    public egg: string;
+    public tomatoes: string;
+    public pepper: string;
+    public yellowCheese: string;
+    public whiteCheese: string;
+
+    constructor(flour: string, milk: string, oil: string, salt: string, sugar: string, egg: string, tomatoes: string, pepper: string, yellowCheese: string, whiteCheese: string){
+        this.flour = flour;
+        this.milk = milk;
+        this.oil = oil;
+        this.salt = salt;
+        this.sugar = sugar;
+        this.egg = egg;
+        this.tomatoes = tomatoes;
+        this.pepper = pepper;
+        
+}
+class Recept  {
+    constructor(public name: string, public source: string, public ingredients: ListOfIIngredients ){
+
+    }
+}
